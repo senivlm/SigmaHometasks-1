4353bd37c1a579eea06638b2003916d330eaeea0
Andrew <37120451+pumpykyt@users.noreply.github.com>
2022-10-26 18:59:43 +0300

    refactor lab 1 main

--- Hometask2/Program.cs
+++ Hometask2/Program.cs
@@ -1,9 +1,5 @@
-﻿using Hometask2;
-using Hometask2.Implementations;
+﻿using Hometask2.Implementations;
 using Hometask2.Interfaces;
 
-IProblemSolver problem1 = new Problem1Solver();
-IProblemSolver problem2 = new Problem2Solver();
-
-problem1.Solve();
-problem2.Solve();
+var problems = new List<IProblemSolver> { new Problem1Solver(), new Problem2Solver() };
+foreach (var problem in problems) problem.Solve();
