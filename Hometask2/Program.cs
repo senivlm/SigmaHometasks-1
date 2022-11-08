92f4de1c94bbc91badc322c08f2c4a0928310b45
senivlm <91743906+senivlm@users.noreply.github.com>
2022-11-08 15:39:32 +0200

    Update Program.cs

--- Hometask2/Program.cs
+++ Hometask2/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Hometask2.Implementations;
 using Hometask2.Interfaces;
-
+// не стандартна подача, але є нюанси з реалізацією. Уточню усно.
 var problems = new List<IProblemSolver> { new Problem1Solver(), new Problem2Solver() };
 foreach (var problem in problems) problem.Solve();
